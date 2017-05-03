81b172671943aeaac35b660e08a3fa9f5a9f4520
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:48:05 -0300

    network creation process moved to this script

--- build-docker-alarm-db.sh
+++ build-docker-alarm-db.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-set -a
-set -e
-set -u
+#
+# A simple script to build a container for the alarm server's database.
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
 
-source env-vars.sh
+. ./env-vars.sh
 
-./build-volume.sh
+### Creates a new storage volume for the container if it doesn't exist ###
 
-DOCKER_VOLUMES=$(docker network ls | grep ${NETWORK_ID})
-if [ -z "${DOCKER_VOLUMES}" ]; then
-    docker network create ${NETWORK_ID}
+# Checks if the volume already exists
+DOCKER_DATA_CONTAINERS=$(docker ps -a | grep ${DOCKER_VOLUME_NAME})
+# If not, it is created
+if [ -z "${DOCKER_DATA_CONTAINERS}" ]; then
+    echo 'Storage volume has not been found. Creating a new one ...'
+    docker create -v ${PGDATA} --name ${DOCKER_VOLUME_NAME} postgres &> /dev/null
+fi
+
+### Creates a new internal network if it doesn't exist ###
+DOCKER_NETWORKS=$(docker network ls | grep ${NETWORK_ID})
+if [ -z "${DOCKER_NETWORKS}" ]; then
+    echo 'Creating a new network...'
+    docker network create ${NETWORK_ID} &> /dev/null
 fi
 
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME} .
