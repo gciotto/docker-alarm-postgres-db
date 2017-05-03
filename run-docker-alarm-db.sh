37174d5b65fcf4b803c21963be8eab861f401fad
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:49:10 -0300

    commentaries and more verbose

--- run-docker-alarm-db.sh
+++ run-docker-alarm-db.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-set -a
-set -e
-set -u
+#
+# An auxiliary script to start a new Docker container. It should be used to test the 
+# respective image. To deploy, use the composed repository, available in
+# https://github.com/lnls-sirius/docker-alarm-composed .  
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
 
-source env-vars.sh
+. ./env-vars.sh
 
+# Verifies if a container with the same name is already running.
 CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
-if [ ! -z "$CONTAINERS" ]; then
-    docker stop ${DOCKER_RUN_NAME}
-    docker rm ${DOCKER_RUN_NAME}
+if [ ! -z ${CONTAINERS:+x} ]; then
+
+    echo "A container with the name ${DOCKER_RUN_NAME} is already running..."
+
+    # Stops running container and deletes it.
+    echo "Executing 'docker stop ${DOCKER_RUN_NAME}' ..."
+    docker stop ${DOCKER_RUN_NAME} &> /dev/null
+
+    echo "Executing 'docker rm ${DOCKER_RUN_NAME}' ..."
+    docker rm ${DOCKER_RUN_NAME} &> /dev/null
 fi
 
 docker run -d --name=${DOCKER_RUN_NAME} -e POSTGRES_USER=${POSTGRES_USER} \
