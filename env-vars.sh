fd3048aeed7244bd032ca87bbb8d27d8d254450d
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 14:47:25 -0300

    commentaries added

--- env-vars.sh
+++ env-vars.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Essential environment variables
+
 DOCKER_MANTAINER_NAME=gciotto
 DOCKER_NAME=alarm-postgres-db
 DOCKER_RUN_NAME=alarm-postgres-db
 DOCKER_VOLUME_NAME=alarm-database-volume
+
+# Postgres variables
 
 POSTGRES_USER=lnls_alarm_user
 POSTGRES_PASSWORD=controle
